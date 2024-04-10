db683b422e42418a95a4c23bb50b9d9ebe98ed38
FelipeMartins <Felipe.martins@edu.unifor.br>
2024-04-10 10:44:29 -0300

    Pagina de cadastro 90% pronta

--- src/Componentes/ContainerCadastro/Cadastro.jsx
+++ src/Componentes/ContainerCadastro/Cadastro.jsx
@@ -1,16 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../ContainerCadastro/ContainerCadastro.css';
 import CodeAcadamyLogoSemFundo from '../../Assets/Imagens/CodeAcademyLogoSemFundo.png';
 import google from '../../Assets/Imagens/SignGoogle.png';
 import { useNavigate } from 'react-router-dom';
+import server from "../../server.json";
 
 function Cadastro() {
-
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
         document.title = "Cadastro";
     }, []);
+
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        // Verifica se a senha coincide com a confirmação de senha
+        if (password !== confirmPassword) {
+            alert("As senhas não coincidem");
+            return;
+        }
+
+        const newUser = {
+            id: server.usuarios.length + 1, 
+            nome: username,
+            email: email,
+            senha: password
+        };
+
+        try {
+            // Faz uma solicitação POST para adicionar o novo usuário ao servidor JSON
+            const response = await fetch('http://localhost:3001/usuarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            });
+
+            if (response.ok) {
+                alert('Usuário cadastrado com sucesso!');
+                navigate('/CatalagodeCursos');
+            } else {
+                alert('Ocorreu um erro ao cadastrar o usuário.');
+            }
+        } catch (error) {
+            console.error('Erro ao enviar solicitação POST:', error);
+            alert('Ocorreu um erro ao cadastrar o usuário.');
+        }
+    };
 
     return (
         <>
@@ -20,17 +65,16 @@ function Cadastro() {
                         <img src={CodeAcadamyLogoSemFundo} alt="" />
                         <h2>Cadastro</h2>
                     </div>
-                    <form action="">
-                        <label htmlFor="username">Digite seu E-mail:</label>
-                        <input type="text" id="username" name="username" placeholder="Digite seu E-mail" />
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">Digite seu E-mail:</label>
+                        <input type="text" id="email" name="email" placeholder="Digite seu E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
                         <label htmlFor="username">Nome de Usuario:</label>
-                        <input type="text" id="username" name="username" placeholder="Digite seu Nome de Usuario" />
+                        <input type="text" id="username" name="username" placeholder="Digite seu Nome de Usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
                         <label htmlFor="password">Senha:</label>
-                        <input type="password" id="password" name="password" placeholder="Digite sua senha" />
+                        <input type="password" id="password" name="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} />
                         <label htmlFor="confirmPassword">Confirme sua Senha:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirme sua senha" />
-                        <input type="submit" onClick={() =>   navigate('/') } value="Cadastrar" className="button_registrar_cadastro" />
-
+                        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirme sua senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <input type="submit" value="Cadastrar" className="button_registrar_cadastro" />
                     </form>
                     <div className="Sign_modes">
                         <img src={google} alt="" />
