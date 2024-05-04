17bed79851bbe8970ffe66f3704705ca331e8196
PedroVictorMcB <pedrovictormcb80@gmail.com>
2024-05-04 09:35:08 -0300

    sincronização 1.0

--- src/Componentes/ContainerCadastro/Cadastro.jsx
+++ src/Componentes/ContainerCadastro/Cadastro.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import '../ContainerCadastro/ContainerCadastro.css';
-import CodeAcademyLogoSemFundo from '../../Assets/Imagens/CodeAcademyLogoSemFundo.png';
-import google from '../../Assets/Imagens/SignGoogle.png';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "../ContainerCadastro/ContainerCadastro.css";
+import CodeAcademyLogoSemFundo from "../../Assets/Imagens/CodeAcademyLogoSemFundo.png";
+import google from "../../Assets/Imagens/SignGoogle.png";
+import { useNavigate } from "react-router-dom";
 import server from "../../server.json";
 
 function Cadastro() {
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [userType, setUserType] = useState('aluno'); // Estado para controlar o tipo de usuário
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [userType, setUserType] = useState("aluno"); // Estado para controlar o tipo de usuário
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -31,28 +31,28 @@ function Cadastro() {
             nome: username,
             email: email,
             senha: password,
-            tag: userType // Usa o estado 'userType' para definir o tipo de usuário
+            tag: userType, // Usa o estado 'userType' para definir o tipo de usuário
         };
 
         try {
             // Faz uma solicitação POST para adicionar o novo usuário ao servidor JSON
-            const response = await fetch('http://localhost:3001/usuarios', {
-                method: 'POST',
+            const response = await fetch("http://localhost:3001/usuarios", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(newUser)
+                body: JSON.stringify(newUser),
             });
 
             if (response.ok) {
-                alert('Usuário cadastrado com sucesso!');
-                navigate('/CatalogodeCursos');
+                alert("Usuário cadastrado com sucesso!");
+                navigate("/catalogoDeCursos");
             } else {
-                alert('Ocorreu um erro ao cadastrar o usuário.');
+                alert("Ocorreu um erro ao cadastrar o usuário.");
             }
         } catch (error) {
-            console.error('Erro ao enviar solicitação POST:', error);
-            alert('Ocorreu um erro ao cadastrar o usuário.');
+            console.error("Erro ao enviar solicitação POST:", error);
+            alert("Ocorreu um erro ao cadastrar o usuário.");
         }
     };
 
@@ -66,27 +66,72 @@ function Cadastro() {
                     </div>
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="email">Digite seu E-mail:</label>
-                        <input type="text" id="email" name="email" placeholder="Digite seu E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Digite seu E-mail"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                         <label htmlFor="username">Nome de Usuario:</label>
-                        <input type="text" id="username" name="username" placeholder="Digite seu Nome de Usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Digite seu Nome de Usuario"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                         <label htmlFor="password">Senha:</label>
-                        <input type="password" id="password" name="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <label htmlFor="confirmPassword">Confirme sua Senha:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirme sua senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite sua senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label htmlFor="confirmPassword">
+                            Confirme sua Senha:
+                        </label>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Confirme sua senha"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
 
                         {/* Campo select para escolher o tipo de usuário */}
                         <label htmlFor="userType">Tipo de Usuário:</label>
-                        <select id="userType" name="userType" value={userType} onChange={(e) => setUserType(e.target.value)}>
+                        <select
+                            id="userType"
+                            name="userType"
+                            value={userType}
+                            onChange={(e) => setUserType(e.target.value)}
+                        >
                             <option value="aluno">Aluno</option>
                             <option value="professor">Professor</option>
                         </select>
 
-                        <input type="submit" value="Cadastrar" className="button_registrar_cadastro" />
+                        <input
+                            type="submit"
+                            value="Cadastrar"
+                            className="button_registrar_cadastro"
+                        />
                     </form>
                     <div className="Sign_modes">
                         <img src={google} alt="" />
-                        <img src="/code-acadamy/src/Assets/Imagens/SignApple.png" alt="" />
-                        <img src="/code-acadamy/src/Assets/Imagens/Signfacebook.png" alt="" />
+                        <img
+                            src="/code-acadamy/src/Assets/Imagens/SignApple.png"
+                            alt=""
+                        />
+                        <img
+                            src="/code-acadamy/src/Assets/Imagens/Signfacebook.png"
+                            alt=""
+                        />
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main'
-import CadastroPage from './pages/CadastroPage';
-import LoginPage from './pages/LoginPage';
-import CatalogoCursos from './pages/CatalogoCursos';
-import CursoAdicionar from './pages/ProfessorPage.jsx';
-import Cursos from './pages/Cursos/Cursos';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import CadastroPage from "./pages/CadastroPage";
+import LoginPage from "./pages/LoginPage";
+import CatalogoCursos from "./pages/CatalogoCursos";
+import CursoAdicionar from "./pages/ProfessorPage.jsx";
+import Cursos from "./pages/Cursos/Cursos";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<Main />} />
-        <Route path="cadastro" element={<CadastroPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="CatalagodeCursos" element={<CatalogoCursos />} />
-        <Route path="CursoAdicionar" element={<CursoAdicionar/>}/>
-        <Route path="cursos" element={<Cursos />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="/" element={<Main />} />
+                <Route path="cadastro" element={<CadastroPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="catalogoDeCursos" element={<CatalogoCursos />} />
+                <Route path="CursoAdicionar" element={<CursoAdicionar />} />
+                <Route path="cursos" element={<Cursos />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
 reportWebVitals();
